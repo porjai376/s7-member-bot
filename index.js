0f457bbc4d98c5be658085d5ae7a738b407b71ed
porjai376 <porjai376@gmail.com>
2026-05-10 22:16:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4391,11 +4391,15 @@ try {
     msg += `📘 INFO [${pid}] [DTAC]\n`;
     msg += `_  _  _  _  _  _  _  _  _  _  _\n`;
 
-    const dtacResult =
+   let dtacResult =
   dData?.data?.body?.result ||
   dData?.body?.result ||
   dData?.result ||
   {};
+
+if (Array.isArray(dtacResult)) {
+  dtacResult = dtacResult[0] || {};
+}
 
 msg += `ชื่อ-สกุล: ${dtacResult?.userData?.NameSurname || '-'}\n`;
 msg += `เลขบัตร: *********${String(pid).slice(-4)}\n\n`;
@@ -4405,20 +4409,35 @@ const prepaid =
     ? dtacResult.subscribers.prepaid
     : [];
 
+const postpaid =
+  Array.isArray(dtacResult?.subscribers?.postpaid)
+    ? dtacResult.subscribers.postpaid
+    : [];
+
 if (prepaid.length) {
 
-  msg += `📘 เบอร์เติมเงิน (Prepaid):\n`;
+  msg += `📱 เบอร์เติมเงิน (Prepaid):\n`;
 
   prepaid.forEach((item, i) => {
-    msg += `${i + 1}.${item.number || '-'} (${item.aou || '-'})\n`;
+    msg += `${i + 1}. ${item.number || '-'} (${item.aou || '-'})\n`;
   });
 
-  msg += `_ _ _ _ _ _ _ _ _ _ _\n`;
+  msg += `────────────────\n`;
 }
-  }
 
-} catch (e) {
-  console.log('all% dtac error:', e.message);
+if (postpaid.length) {
+
+  msg += `📱 เบอร์รายเดือน (Postpaid):\n`;
+
+  postpaid.forEach((item, i) => {
+    msg += `${i + 1}. ${item.number || '-'} (${item.aou || '-'})\n`;
+  });
+
+  msg += `────────────────\n`;
+}
+
+if (!prepaid.length && !postpaid.length) {
+  msg += `❌ ไม่พบเบอร์ที่ลงทะเบียน\n`;
 }
 
       msg += `\n🔎ข้อมูลบุคคล/สิทธิรักษา\n`;
